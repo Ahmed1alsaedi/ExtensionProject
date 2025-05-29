50ba6493bf556a40aef5bf07fe340a387aa3a4c9
Ahmed <Ams_jed24@hotmail.com>
2025-05-29 21:33:42 +0300

    a

--- test.js
+++ test.js
@@ -1,10 +1,44 @@
+const container = document.querySelector(".container");
+const card = document.querySelector(".card");
+const left = document.querySelector(".left-sec");
+const right = document.querySelector(".right-sec");
 async function getData(){
     try{
         const response = await fetch("assest/data/data.json");
         const data = await response.json();
-        console.log(data)
+        return data;
     } catch(err){
         console.log(err);
     }
 }
-getData();
+
+
+async function getAll(){
+    try{
+    const data = await getData();
+    data.forEach(el =>{
+       const newCard = document.createElement("div")
+       newCard.classList.add("card")
+       const newLeft = document.createElement("div")
+       newLeft.classList.add("left-sec")
+       const newRight = document.createElement("div")
+       newRight.classList.add("right-sec")
+       container.appendChild(newCard);
+       newCard.appendChild(newLeft);
+       newCard.appendChild(newRight)
+       const newImg = document.createElement("img")
+       let log = el.logo
+       newImg.src= log.substring(1)
+
+       const newBtn = document.createElement("button")
+       newBtn.classList.add("statBtns")
+       newLeft.appendChild(newImg);
+       newLeft.appendChild(newBtn);
+    })
+    } catch(err){
+       console.log(err);
+    }
+    
+    
+}
+getAll();
